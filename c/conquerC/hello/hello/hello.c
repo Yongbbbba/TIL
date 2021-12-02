d4df6f82387a12d5844f71f4e13a59e613d0daf6
yongbbbba <lisagamer123@gmail.com>
2021-12-02 22:30:47 +0900

    씹어먹는 C: chapter10 연습문제

--- c/conquerC/hello/hello/hello.c
+++ c/conquerC/hello/hello/hello.c
@@ -2,25 +2,16 @@
 #include <stdio.h>
 
 int main() {
-	int cnt;
-	int total_score = 0;
-	printf("몇 개의 과목 점수를 입력 받을 것인가요?");
-	scanf("%d", &cnt);
-	
-	printf("\n");
-	printf("각 과목의 점수를 입력해 주세요\n");
-	for (int i = 0; i < cnt; i++)
+	// chapter10
+	// 소수점 둘째자리까지 출력
+	double f;
+	while (1)
 	{
-		int score;
-		printf("과목 %d : ", i+1);
-		scanf("%d", &score);
+		printf("값을 입력하시오 : ");
+		scanf("%lf", &f);
 		printf("\n");
-		total_score += score;
+		printf("출력: %d\n", (int)(f * 100) % 100);
 	}
-
-	double result = (double)total_score / cnt;
-	printf("전체 과목의 평균은 : %lf", result);
-
 
 	return 0;
 }
