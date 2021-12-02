7114854b3d267a1a6e1d1a6bb7e063cd1ac63cd8
yongbbbba <lisagamer123@gmail.com>
2021-12-02 23:04:16 +0900

    씹어먹는 C: chapter11-1 연습문제

--- c/conquerC/hello/hello/hello.c
+++ c/conquerC/hello/hello/hello.c
@@ -1,17 +1,57 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
+#include <stdlib.h>
 
 int main() {
-	// chapter10
-	// 소수점 둘째자리까지 출력
-	double f;
-	while (1)
+	// chapter 11
+	// 문제 1
+	int size;
+	printf("몇 명의 학생 성적을 입력할 것인가요? : ");
+	scanf("%d", &size);
+	printf("\n");
+	int* scores = (int*)malloc(sizeof(int) * size);
+	for (int i = 1; i <= size; i++)
 	{
-		printf("값을 입력하시오 : ");
-		scanf("%lf", &f);
+		int score;
+		printf("%d번째 학생의 성적을 입력하세요 : ", i);
+		scanf("%d", &score);
 		printf("\n");
-		printf("출력: %d\n", (int)(f * 100) % 100);
+		scores[i - 1] = score;
 	}
+	printf("입력 종료\n");
+
+	// 정렬
+	for (int i = 0; i < size; i++)
+	{
+		for (int j = i; j < size; j++)
+		{
+			if (scores[i] > scores[j])
+			{
+				int temp = scores[i];
+				scores[i] = scores[j];
+				scores[j] = temp;
+			}
+		}
+	}
+	// 출력
+	for (int i = 0; i < size; i++)
+	{
+		printf("%d ", scores[i]);
+	}
+	printf("\n");
+
+	// 문제2
+	for (int i = 0; i < size; i++)
+	{
+		printf("%d번 학생       ", i + 1);
+		for (int j = 0; j < scores[i]; j++)
+		{
+			printf("*");
+		}
+		printf("\n");
+	}
+
+	free(scores);
 
 	return 0;
 }
