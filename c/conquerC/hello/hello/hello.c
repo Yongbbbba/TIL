bf50c8a666c4246e6189ea98fe5a888320517651
yongbbbba <lisagamer123@gmail.com>
2021-12-11 11:19:19 +0900

    씹어먹는 C: chapter 13-3 생각해보기 문제2

--- c/conquerC/hello/hello/hello.c
+++ c/conquerC/hello/hello/hello.c
@@ -18,53 +18,15 @@ void arr_swap(int *a, int *b)
 }
 
 int main() {
-	// 문제1
-	int scores[5][4];
-	int total_average = 0;
-	
-	//// 입력받기
-	for (int i = 0; i < 5; i++)
+	// 문제2
+	int a = 136, b = 736;
+	while (a)
 	{
-		char* subjects[3] = { "수학", "국어", "영어" };
-		int average = 0;
-		for (int j = 0; j < 3; j++)
-		{
-			printf("%d번 학생의 %s 점수를 입력 하세요 : ", i+1, subjects[j]);
-			int score;
-			scanf("%d", &score);
-			printf("\n");
-			scores[i][j] = score;
-			average += score;
-		}
-		scores[i][3] = average / 3;
-		total_average += scores[i][3];
-	}
-
-	total_average /= 5;
-
-	//// 정렬하기   - 버블
-	for (int i = 0; i < 5; i++)
-	{
-		for (int j = i; j < 5; j++)
-		{
-			if (scores[i][3] > scores[j][3])
-			{
-				arr_swap(scores[i], scores[j]);
-			}
-		}
-	}
-
-	//// 출력하기
-	for (int i = 0; i < 5; i++)
-	{
-		char* result;
-		if (scores[i][3] >= total_average)
-			result = "합격";
-		else
-			result = "불합격";
-
-		printf("%d번 학생의 평균은 %d입니다 -> %s\n", i + 1, scores[i][3], result);
+		int temp = b % a; 
+		b = a;
+		a = temp;
 	}
 	
+
 	return 0;
 }
