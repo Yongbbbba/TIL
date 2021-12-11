ea56a86eb3dcf2582d9b1fb69082201c1f2243ee
yongbbbba <lisagamer123@gmail.com>
2021-12-11 16:22:20 +0900

    씹어먹는 C: chapter 13-3 생각해보기 문제4

--- c/conquerC/hello/hello/hello.c
+++ c/conquerC/hello/hello/hello.c
@@ -1,11 +1,70 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdbool.h>
 
 // chapter 13-3
 // 생각해보기
 
 // 배열 swap
+void arr_swap(int* a, int* b);
+// factorial
+int fact(int num);
+// calculator
+int cal();
+
+int main() {
+	// 문제4
+	// 숫자, +, -가 아닌 경우 다시 입력할 것
+	// 지우기 기능
+	// 계산 결과 누적
+	cal();
+
+
+	return 0;
+}
+
+int cal()
+{
+	printf("계산기 프로그램 시작\n");
+	int now = 0;
+	int input_num;
+	int input_oper;
+	printf("%d\n", now);
+	while (1)
+	{
+		printf("\n숫자를 더하려면 1, 빼려면 2, 종료하려면 3을 눌러주세요 : ");
+		int result = scanf("%d", &input_oper);
+		if (!result)
+		{
+			printf("다시 입력해주세요\n");
+			continue;
+		}
+
+		if (input_oper == 3)
+		{
+			printf("계산기 프로그램을 종료합니다\n");
+			return 0;
+		}			
+
+		else
+		{
+			printf("숫자를 입력해주세요 : ");
+			scanf("%d", &input_num);
+			if (input_oper == 1)
+			{
+				now += input_num;
+				printf("\n%d\n", now);
+			}
+			else
+			{
+				now -= input_num;
+				printf("\n%d\n", now);
+			}
+		}
+	}
+}
+
 void arr_swap(int *a, int *b)
 {
 	int temp[4];
@@ -28,12 +87,4 @@ int fact(int num)
 	int result = num * fact(num - 1);
 
 	return result;
-}
-
-int main() {
-	// 문제3
-	int result = fact(5);
-	
-
-	return 0;
 }
